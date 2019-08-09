f81fcafb0c04548a8e4b6bdefeb6db5d3af52ff1
ajha <ajha@LTPC0K7AG1.corp.hsdyn.com>
2019-08-09 12:21:12 +0530

    FIrst Test case

---
+++ SeleniumBasicScript/src/main/java/seleniumBasicCode/LaunchApplication.java
@@ -0,0 +1,16 @@
+package seleniumBasicCode;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchApplication {
+	@Test
+	public void launchApplication() {
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+
+}
